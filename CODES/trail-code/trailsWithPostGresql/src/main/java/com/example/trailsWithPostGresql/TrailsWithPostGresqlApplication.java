e4f94a1f8d36bd987c88d82cb80ef9aa324c385e
Tushar-Nagdive-Dev <codewithtushar840@gmail.com>
2024-09-05 18:58:46 +0530

    add trail project

---
+++ CODES/trail-code/trailsWithPostGresql/src/main/java/com/example/trailsWithPostGresql/TrailsWithPostGresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.trailsWithPostGresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailsWithPostGresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailsWithPostGresqlApplication.class, args);
+	}
+
+}
