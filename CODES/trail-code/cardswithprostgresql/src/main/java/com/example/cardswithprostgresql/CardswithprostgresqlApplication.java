01e92f95c0c40e9d311036dd6d86812805fe1267
Tushar-Nagdive-Dev <codewithtushar840@gmail.com>
2024-09-08 18:43:25 +0530

    add dependend

--- CODES/trail-code/cardswithprostgresql/src/main/java/com/example/cardswithprostgresql/CardswithprostgresqlApplication.java
+++ CODES/trail-code/cardswithprostgresql/src/main/java/com/example/cardswithprostgresql/CardswithprostgresqlApplication.java
@@ -2,8 +2,36 @@ package com.example.cardswithprostgresql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@eazybytes.com",
+						url = "https://www.eazybytes.com"
+				),
+				license = @License(
+						name = "Udemy 2.0",
+						url = "https://www.eazybytes.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Cards microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
 public class CardswithprostgresqlApplication {
 
 	public static void main(String[] args) {
