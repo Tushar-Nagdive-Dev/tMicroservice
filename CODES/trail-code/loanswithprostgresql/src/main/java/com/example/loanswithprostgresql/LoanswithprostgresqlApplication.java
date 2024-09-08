9543b031511b18687998d44e4cc49401bf5a51ea
Tushar-Nagdive-Dev <codewithtushar840@gmail.com>
2024-09-08 16:09:16 +0530

    add microservice for loans

---
+++ CODES/trail-code/loanswithprostgresql/src/main/java/com/example/loanswithprostgresql/LoanswithprostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.loanswithprostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanswithprostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanswithprostgresqlApplication.class, args);
+	}
+
+}
