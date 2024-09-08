53692db2aac1ba8b66adbbab9ea02e9ced853fb2
Tushar-Nagdive-Dev <codewithtushar840@gmail.com>
2024-09-08 15:50:41 +0530

    add cards microservice with pistgress and liquibase migration

---
+++ CODES/trail-code/cardswithprostgresql/src/main/java/com/example/cardswithprostgresql/CardswithprostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardswithprostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardswithprostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardswithprostgresqlApplication.class, args);
+	}
+
+}
