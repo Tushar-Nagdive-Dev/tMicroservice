c6845ce7e8c6440da7e980080d6b0f404b7d70a8
Tushar-Nagdive-Dev <codewithtushar840@gmail.com>
2024-09-15 12:38:01 +0530

    this is 2 version and in this version including common docker-compose for production ready build

---
+++ CODES/trail-code-v2/accountwithpostgresql/src/main/java/com/example/accountmswithpostgresql/mapper/AccountMapper.java
@@ -0,0 +1,21 @@
+package com.example.accountmswithpostgresql.mapper;
+
+import com.example.accountmswithpostgresql.dto.AccountDto;
+import com.example.accountmswithpostgresql.entities.Accounts;
+
+public class AccountMapper {
+	
+	public static AccountDto mapToAccountsDto(Accounts accounts, AccountDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+}
---
+++ CODES/trail-code-v2/cardswithprostgresql/src/main/java/com/example/cardswithprostgresql/controller/CardConfigController.java
@@ -0,0 +1,63 @@
+package com.example.cardswithprostgresql.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.cardswithprostgresql.dto.CardServiceInfoDto;
+import com.example.cardswithprostgresql.dto.ErrorResponseDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(
+        name = "CRUD REST APIs for Cards Configd",
+        description = "Get all Cards major configuration"
+)
+@RestController
+@RequestMapping(path = "/config", produces = {MediaType.APPLICATION_JSON_VALUE})
+public class CardConfigController {
+
+	@Value("${build.name}")
+	private String buildName;
+	
+	@Value("${build.version}")	
+	private String buildVersion;
+	
+	@Autowired
+	private CardServiceInfoDto cardServiceInfoDto;
+	
+	@Operation(summary = "Get Build information", description = "Get build informations that is deployed into cards microservices")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+		@ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(
+			schema = @Schema(implementation = ErrorResponseDto.class)
+		))
+	})
+	@GetMapping("/build-info")
+	public ResponseEntity<String> getBuildVersion() {
+		return ResponseEntity.status(HttpStatus.OK).body("Build Name: " + buildName + ", Build Version: " + buildVersion);
+	}
+	
+	@Operation(summary = "Get Cards Info", description = "Get Cards informations that is deployed into card microservices")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+		@ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(
+			schema = @Schema(implementation = ErrorResponseDto.class)
+		))
+	})
+	@GetMapping("/card-info")
+	public ResponseEntity<CardServiceInfoDto> getContactInfo() {
+		return ResponseEntity.status(HttpStatus.OK).body(cardServiceInfoDto);
+	}
+}
