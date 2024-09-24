678014864c7fa106a88f4c77cac66f4564bb0eda
Tushar-Nagdive-Dev <codewithtushar840@gmail.com>
2024-09-24 17:12:52 +0530

    add code

---
+++ CODES/trail-code-service-discovery/buid-docker-image.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+echo "configserver build ...." 
+cd configserver/
+./mvnw compile jib:dockerBuild
+
+echo "\n"
+cd ../eurekaserver/
+./mvnw compile jib:dockerBuild
+
+echo "\n"
+echo "accounts"
+cd ../accountwithpostgresql/
+./mvnw compile jib:dockerBuild
+
+echo "\n"
+echo "cards"
+cd ../cardswithprostgresql/
+./mvnw compile jib:dockerBuild
+
+echo "\n"
+echo "loans"
+cd ../loanswithprostgresql/
+./mvnw compile jib:dockerBuild
